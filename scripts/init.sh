378d218eb3c9a832adb19cb0e8cd75883a17f058
kpolischuk <kostiantyn.polishchuk@ria.com>
2018-10-17 15:46:42 +0300

    Все переписав

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-["cat .gitignore | grep 'editor-config/'"] && sed -i -e '$a\' .gitignore  && echo 'editor-config/*' >> .gitignore;\
-npm i -D husky; ["ls -a | grep .huskyrc" == 'null'] && ln -s editor-config/.huskyrc;\
-npm i -D -E prettier && npm i -D pretty-quick; ["ls -a | grep .prettierrc" == 'null'] && ln -s editor-config/.prettierrc
+[ "cat .gitignore | grep 'editor-config/'" ] && sed -i -e '$a\' .gitignore  && echo 'editor-config/*' >> .gitignore;\
+npm i -D husky; [ "ls -a | grep .huskyrc" == 'null' ] && ln -s editor-config/.huskyrc;\
+npm i -D -E prettier && npm i -D pretty-quick; [ "ls -a | grep .prettierrc" == 'null' ] && ln -s editor-config/.prettierrc
